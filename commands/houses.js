93c631131dff07dba1c4bce0e06c72b4cca7ba4d
Noah Braam <nwbraam@gmail.com>
2019-02-03 18:53:00 -0500

    added more to houses command

--- commands/houses.js
+++ commands/houses.js
@@ -2,6 +2,7 @@ module.exports = {
 	name: 'houses',
 	description: 'Info about the different houses',
 	execute(message, args) {
-		message.channel.send('There are 4 different houses: Gryffindor, Hufflepuff, Ravenclaw and Slytherin');
+        message.channel.send('There are 4 different houses: Gryffindor, Hufflepuff, Ravenclaw and Slytherin');
+        message.channel.send('To learn more about a particular house, use !slytherin, !ravenclaw, !hufflepuff or !gryffindor');
 	},
 };
